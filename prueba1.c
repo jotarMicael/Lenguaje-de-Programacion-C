9e5d2a0b8a4237057d1c69167be994818152a30b
jotarMicael <gameuxlegacy@gmail.com>
2020-05-14 18:49:31 -0300

    Update prueba1.c

--- prueba1.c
+++ prueba1.c
@@ -1,7 +1,35 @@
-#include <limits.h> // Para usar UINT_MAX
-char main()
-{
-char c;
-c = 'A' + 2;
-return (c);
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+
+int c,cant,espacios;
+
+cant= 0;
+espacios=0;
+c=getchar();
+while (c != EOF) {
+		if (c == '\n')
+			espacios++;
+
+		else
+		cant++;
+		
+        putchar(c);
+		c=getchar();
+
+
+
+
+}
+
+printf("la cantidad de caracteres es: %d\n", cant);
+printf("la cantidad de saltos de lineas es: %d\n", espacios );
+
+
+
+
+
+
 }
